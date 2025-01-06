7a0f222338c442f55966baa47c67db0895d67cab
Techieflo <87770281+Techieflo@users.noreply.github.com>
2025-01-06 21:05:47 +0100

    Update app.py now

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import yt_dlp
 from flask import Flask, request, jsonify
-import subprocess
 import json
 import requests
 from urllib.parse import urlparse, parse_qs
@@ -23,9 +22,31 @@ def clean_youtube_url(url):
     else:
         return None
 
+# Hardcoded cookies in Python dictionary format
+hardcoded_cookies = {
+    "LOGIN_INFO": "AFmmF2swRgIhAI86xsvyd5KQK0pGJWm3NyjBIJHMcodr9UBEBzdRaQRjAiEAkUt_uDG6y_mYiZJ6dM7NRkscbXbJ_DVFqA7qK42Ygqk:QUQ3MjNmd01UZHV3MkNQOWFCQlVCRW1jbWJrM1ROUEpNZWdNa1lrLU9zRTFZS1NYeGNTU1NqYW1GM1kxdWEzcWRCNWNWMC1td3hickVjUnd3Ym9RMXpqbjJGSFU0TGpjeVpDWFNFOFBGeU1KTDJaODBjdmJzSHZnQ1Z6NzJYT1Iya0ktSzlHY0Y5eVVwZ3NScmRtM1dzNFVPZkZjZE4yeWJR",
+    "PREF": "tz=Africa.Lagos&f4=4000000&f7=100",
+    "SID": "g.a000rwgIdIQ6zwLoBYiwWzn0pb3fRfaT8DwGWctqan_WsH46id4MU8MnM9S9L6m6A6AhTDfbHwACgYKAe4SARcSFQHGX2MipAkOdNCEBe5S1G5swftrShoVAUF8yKpodDKhhscbVt1XgpCoxaiP0076",
+    "__Secure-1PSID": "g.a000rwgIdIQ6zwLoBYiwWzn0pb3fRfaT8DwGWctqan_WsH46id4MaejYi6vqgyVlbb4OX5cEqAACgYKAToSARcSFQHGX2MiuP-KOtrAHdZz_kG5LhWmLBoVAUF8yKpeCCUVuvOcXgLE987_-v0S0076",
+    "__Secure-3PSID": "g.a000rwgIdIQ6zwLoBYiwWzn0pb3fRfaT8DwGWctqan_WsH46id4MDGKxK65RTPrZg5gQMJq7PQACgYKAXsSARcSFQHGX2MiMfDiSrSl6jhww7mp5GGp_xoVAUF8yKoo2DluJjVedfN6brlOBMiT0076",
+    "HSID": "AVk_gJUR-ATs-d5Zb",
+    "SSID": "AkQrJUyS7O7ifzmvY",
+    "APISID": "InIWZ9itO8Eq-Rtd/AvtNjnRxlYXvO9Rpy",
+    "SAPISID": "m-HPzqdDOHqgAloj/A_d8J5FxrxYOvmEO2",
+    "__Secure-1PAPISID": "m-HPzqdDOHqgAloj/A_d8J5FxrxYOvmEO2",
+    "__Secure-3PAPISID": "m-HPzqdDOHqgAloj/A_d8J5FxrxYOvmEO2",
+    "__Secure-1PSIDTS": "sidts-CjIB7wV3sS1O6Vjl9NcKXPplulzI-lplB_dBbYEu8UM2V-MMCICyOfv9P8gIEfKQZXrPYBAA",
+    "__Secure-3PSIDTS": "sidts-CjIB7wV3sS1O6Vjl9NcKXPplulzI-lplB_dBbYEu8UM2V-MMCICyOfv9P8gIEfKQZXrPYBAA",
+    "SIDCC": "AKEyXzVAVaObR4GLJgggikCQ746hXgCRGPmKHLdJHic0lzwVYWSL_921fILl-39T8PLZ4iKT1mA",
+    "__Secure-1PSIDCC": "AKEyXzUDfKlKoJbxzxwT8NO9fWtbapKahCPvoPRx1_-l521boZADREnNzSTLBmyf_MQir6TJqw",
+    "__Secure-3PSIDCC": "AKEyXzUg9HRugNm058NJJd_a-zDxz2FnLCalIIsXdhjPwHFObQPZxsLj7ylYwBce_7Un9hWXcis"
+}
+
 # Function to get video information using yt-dlp (Python wrapper)
 def get_video_info(youtube_url):
     ydl_opts = {'quiet': True, 'format': 'bestvideo+bestaudio/best'}
+    ydl_opts['cookies'] = hardcoded_cookies  # Pass hardcoded cookies directly
+    
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         result = ydl.extract_info(youtube_url, download=False)
         return result
